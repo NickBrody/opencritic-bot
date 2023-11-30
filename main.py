741f36c51bf482161b407586f3df543ecfb052f1
Nikita <deliciousman99@gmail.com>
2023-12-01 02:33:45 +0900

    подправил main файл, добавил виртуальное окружение, создал requirements.txt и env.template

--- main.py
+++ main.py
@@ -34,11 +34,6 @@ def hello(message):
     bot.send_message(message.chat.id, f"Здравствуйте, {name}!")
 
 
-@bot.message_handler(func=lambda message: True)
-def default(message):
-    bot.send_message(message.chat.id, 'Такой команды нет, меня ещё дорабатывают :)')
-
-
 
 @bot.message_handler(commands=['low'])
 def low(message):
@@ -57,7 +52,7 @@ def low_state(message):
     else:
         bot.send_message(message.chat.id, "Неверное число!")
         bot.send_message(message.chat.id, "Введите число от 1 до 12")
-        bot.register_next_step_handler(message, high_state)
+        bot.register_next_step_handler(message, low_state)
 
 
 
@@ -83,25 +78,9 @@ def high_state(message):
 
 
 
-@bot.message_handler(commands=['high'])
-def high(message):
-    bot.send_message(message.chat.id, "Введите количество игр (не больше 12)")
-    bot.set_state(message.from_user.id, States.high, message.chat.id)
-    bot.register_next_step_handler(message, high_state)
-
-@bot.message_handler(state=States.high)
-def high_state(message):
-    user_input_high = int(message.text)
-    if user_input_high > 0 and user_input_high <= 12:
-        print(user_input_high)
-        bot.send_message(message.chat.id, "Лучшие игры текущего года:")
-        result = api.high(user_input_high)
-        json_raw = json.dumps(result, ensure_ascii=False, indent=2)
-        bot.send_message(message.chat.id, f'{result}')
-    else:
-        bot.send_message(message.chat.id, "Неверное число!")
-        bot.send_message(message.chat.id, "Введите число от 1 до 12")
-        bot.register_next_step_handler(message, high_state)
+@bot.message_handler(func=lambda message: True)
+def default(message):
+    bot.send_message(message.chat.id, 'Такой команды нет, меня ещё дорабатывают :)')
 
 
 
